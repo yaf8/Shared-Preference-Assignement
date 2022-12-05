d37461a7aaf2530636a32a752a5b537fddd97571
Yafet Abebe Feleke <yafetema15@gmail.com>
2022-12-05 23:42:08 +0300

    working (save, display, delete all, search and (partially update)) backup 3

--- app/src/main/java/com/example/assigntry/UpdateActivity.java
+++ app/src/main/java/com/example/assigntry/UpdateActivity.java
@@ -10,12 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
 public class UpdateActivity extends AppCompatActivity {
 
-    private Button btnUpdate, btnBack;
+    private Button btnUpdate, btnBack, btnFind;
     private EditText edtUpdate, edtUpdateID, edtUpdateFullName;
     private TextView textSearch;
 
@@ -24,6 +25,7 @@ public class UpdateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
+        btnFind = findViewById(R.id.btnFind);
         btnBack = findViewById(R.id.btnBack);
         btnUpdate = findViewById(R.id.btnUpdate);
         edtUpdate = findViewById(R.id.edtUpdate);
@@ -33,8 +35,12 @@ public class UpdateActivity extends AppCompatActivity {
 
         edtUpdateID.setVisibility(View.INVISIBLE);
         edtUpdateFullName.setVisibility(View.INVISIBLE);
+        btnUpdate.setVisibility(View.INVISIBLE);
 
 
+        btnFind.setOnClickListener(v -> {
+            find();
+        });
 
         btnUpdate.setOnClickListener(v -> {
             update();
@@ -47,16 +53,41 @@ public class UpdateActivity extends AppCompatActivity {
 
     }
 
-    private void update() {
+    private void find() {
 
         String searchID = edtUpdate.getText().toString(), newID = edtUpdateID.getText().toString(), newFullName = edtUpdateFullName.getText().toString();
-        for (StudentModal str : MainActivity.StudentModalArrayList) {
+        for (StudentModal str : MainActivity.StudentModalArrayList)
+        {
             if(Objects.equals(str.getID(), searchID))
             {
                 edtUpdateID.setVisibility(View.VISIBLE);
                 edtUpdateFullName.setVisibility(View.VISIBLE);
+                btnUpdate.setVisibility(View.VISIBLE);
+                Toast.makeText(this, "Found", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    private void update() {
+        String findID = edtUpdate.getText().toString(), newID = edtUpdateID.getText().toString(), newFullName = edtUpdateFullName.getText().toString();
+
+                for(int i = 0; i < MainActivity.StudentModalArrayList.size(); i++)
+                {
+                    if(Objects.equals(MainActivity.StudentModalArrayList.get(i).getID(), findID))
+                    {
+                        MainActivity.StudentModalArrayList.get(i).setID(newID);
+                        MainActivity.StudentModalArrayList.get(i).setFullName(newFullName);
+
+                        Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+                //str.setID(newID);
+                //str.setFullName(newFullName);
+
+
+
     }
 
 
